0814dd0bf0d8eec65fb889b9672babee9db45626
Morte Mors <jefersongb@outlook.com.br>
2022-05-13 23:47:43 -0300

    add author for posts

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
+import { PostAuthor } from './PostAuthor';
 
 export const SinglePostPage = () => {
   const params = useParams();
@@ -23,6 +24,7 @@ export const SinglePostPage = () => {
       <article className="post">
         <h2>{post.title}</h2>
         <p className="post-content">{post.content}</p>
+        <PostAuthor userId={post.user} />
         <Link to={`/editPost/${post.id}`} className="button">
           Edit Post
         </Link>
