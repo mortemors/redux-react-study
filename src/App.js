f55e57d20555cbd2ab9d4eb720fcba8972be4755
Morte Mors <jefersongb@outlook.com.br>
2022-05-13 22:11:12 -0300

    edit posts added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Navbar } from './app/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { PostsList } from './features/posts/PostsList';
 import { AddPostForm } from './features/posts/AddPostForm';
+import { SinglePostPage } from './features/posts/SinglePostPage';
+import { EditPostForm } from './features/posts/EditPostForm';
 
 function App() {
   return (
@@ -19,6 +21,8 @@ function App() {
               </React.Fragment>
             }
           />
+          <Route path="posts/:postId" element={<SinglePostPage />} />
+          <Route path="editPost/:postId" element={<EditPostForm />} />
         </Routes>
       </div>
     </BrowserRouter>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+
+import './index.css';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
